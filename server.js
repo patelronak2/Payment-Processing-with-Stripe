4d57ad11a0457f0893599df715ed85e1623c2550
Ronak Patel <48326102+patelronak2@users.noreply.github.com>
2020-04-14 20:07:34 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,3 @@
-// StAuth10065: 
-// "I Ronak Patel, 000744055 certify that this material is my original work. 
-//  No other person's work has been used without due acknowledgement. I have not made my work available to anyone else."
-
 const express = require('express');
 var app = express();
 var http = require('http').createServer(app);
